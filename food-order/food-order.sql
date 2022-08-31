c5e8dc3fcaf43442b7b974cbda953e900c3bc122
Scelius <111742821+Scelius@users.noreply.github.com>
2022-08-31 02:13:34 +0200

    Add files via upload

---
+++ food-order/food-order.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 31, 2022 at 02:09 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `food-order`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
+(29, 'Admin1', 'Adm1', '2e33a9b0b06aa0a01ede70995674ee23'),
+(32, 'Muamer', 'Muma', '25f9e794323b453885f5181f1b624d0b'),
+(34, 'Admin2', 'Admin2', '25f9e794323b453885f5181f1b624d0b');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `featured` varchar(10) NOT NULL,
+  `active` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
+(28, 'Pizza', 'Food_Category_925.jpg', 'Yes', 'Yes'),
+(29, 'Burger', 'Food_Category_998.jpg', 'Yes', 'Yes'),
+(30, 'MoMo', 'Food_Category_327.jpg', 'Yes', 'Yes'),
+(36, 'Test1', '', 'Yes', 'Yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_food`
+--
+
+CREATE TABLE `tbl_food` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `featured` varchar(10) NOT NULL,
+  `active` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_food`
+--
+
+INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
+(19, 'Dumplings Specials', 'Chicken Dumpling with herbs from mountains.', '5.00', 'Food-Name-6820.jpg', 30, 'Yes', 'Yes'),
+(20, 'Smoky BBQ Pizza', 'Best Firewood Pizza in Town.', '6.00', 'Food-Name-8323.jpg', 28, 'Yes', 'Yes'),
+(25, 'Best Burger', 'Burger of best quality', '10.00', 'Food-Name-3414.jpg', 29, 'Yes', 'Yes'),
+(26, 'Another Burger', 'Another Burger', '10.00', 'Food-Name-4234.jpg', 29, 'Yes', 'Yes'),
+(27, 'Pizza', 'zzz', '10.00', 'Food-Name-1900.jpg', 28, 'Yes', 'Yes'),
+(28, 'Hamburger', 'Najbolji hamburger', '5.00', '', 29, 'Yes', 'Yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `food` varchar(150) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `customer_name` varchar(150) NOT NULL,
+  `customer_contact` varchar(20) NOT NULL,
+  `customer_email` varchar(150) NOT NULL,
+  `customer_address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
+(1, 'Dumplings Specials', '5.00', 2, '10.00', '2022-08-31 12:31:52', 'Delivered', 'Muma', '0688', 'a@a.com', 'Ulica'),
+(2, 'Dumplings Specials', '5.00', 1, '5.00', '2022-08-31 12:42:12', 'Delivered', 'Muamer', '063', 'b@b.com', 'Ulica2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_food`
+--
+ALTER TABLE `tbl_food`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `tbl_food`
+--
+ALTER TABLE `tbl_food`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
